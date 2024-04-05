544b9989043dd082e5eb61ba57fc664147a168fc
GangulyPriti <mondal.18riya@gmail.com>
2024-04-05 20:04:31 +0530

    Day 4 /100

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css',
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'Simple test',
+      'https://www.budgetbytes.com/wp-content/uploads/2018/09/Basic-Chili-in-Bowl-V2-1200-800x1067.jpg'
+    ),
+  ];
+  constructor() {}
+  ngOnIt() {}
+}
