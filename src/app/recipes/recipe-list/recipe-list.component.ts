620b8f42ec09de13eb2b0113001a334f06225246
GangulyPriti <mondal.18riya@gmail.com>
2024-04-05 21:05:24 +0530

    Day 4 /100

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent {
       'Simple test',
       'https://www.budgetbytes.com/wp-content/uploads/2018/09/Basic-Chili-in-Bowl-V2-1200-800x1067.jpg'
     ),
+    new Recipe(
+      'A Test Recipe -2',
+      'Simple test -2',
+      'https://www.budgetbytes.com/wp-content/uploads/2023/05/Pasta-Primavera-V3-800x1067.jpg'
+    ),
   ];
   constructor() {}
   ngOnIt() {}
