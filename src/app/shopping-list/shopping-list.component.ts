03a804bc36c334cbbc50bf16d618677f8c37acc8
GangulyPriti <mondal.18riya@gmail.com>
2024-04-05 21:24:25 +0530

    Day 4 /100

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Ingrediant } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-
+  ingrediants: Ingrediant[] = [
+    new Ingrediant('Apples', 5),
+    new Ingrediant('Tomatoes', 10),
+  ];
+  constructor() {}
 }
